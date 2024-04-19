ef8b2e2dd86f75e723f501b9fea2428ca7be16b0
Járedy Alves <jaredyalves@undefinedname.com>
2024-04-19 20:39:44 +0100

    fix: update rule class name

--- config.def.h
+++ config.def.h
@@ -32,11 +32,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "firefox",  NULL,       NULL,       1 << 0,       0,           -1 },
-	{ "st",       NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "st-256color",       NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "lutris",   NULL,       NULL,       1 << 3,       0,           -1 },
 	{ "steam",    NULL,       NULL,       1 << 3,       0,           -1 },
-	{ "slack",    NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "Slack",    NULL,       NULL,       1 << 4,       0,           -1 },
 };
 
 /* layout(s) */
